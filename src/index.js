989fdf44191dc4e43f6a7e7a7105ea531459a2b8
vicky phung <vickyphung@gmail.com>
2022-09-25 01:46:48 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { ThemeProvider } from './Components/Theme/theme-context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
+
       <ThemeProvider>
         <App />
       </ThemeProvider>
-  </React.StrictMode>
+  
   </BrowserRouter>
 );
 
